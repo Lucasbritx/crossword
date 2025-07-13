11527d8ddcc6a043517a52ea9c8fe4f74d7a07fa
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-13 19:26:42 -0300

    improvements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Crossword from './components/Crossword';
-import { crosswordData } from './crossword-data';
+import { generateCrossword } from './crossword-generator';
 
 function App() {
+  const [crosswordData, setCrosswordData] = useState(null);
+
+  useEffect(() => {
+    setCrosswordData(generateCrossword());
+  }, []);
+
   return (
     <div className="App">
       <h1>Crossword Game</h1>
-      <Crossword data={crosswordData} />
+      {crosswordData && <Crossword data={crosswordData} />}
     </div>
   );
 }
