1c0cd62045e2964dc845f9c4f27dd522659a4684
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-13 18:22:21 -0300

    first prompt create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Crossword from './components/Crossword';
+import { crosswordData } from './crossword-data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Crossword Game</h1>
+      <Crossword data={crosswordData} />
     </div>
   );
 }
